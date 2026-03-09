0d4b84d3f6221d6e8f25884468f2b729362ce624
lukefrederick <129812402+lukefrederick@users.noreply.github.com>
2026-03-09 15:32:12 -0500

    Fixed the arguments in PostReppository and added GetMapping to Postcontroller

--- src/main/java/com/example/jamjudge/controllers/PostController.java
+++ src/main/java/com/example/jamjudge/controllers/PostController.java
@@ -1,4 +1,39 @@
 package com.example.jamjudge.controllers;
 
+import com.example.jamjudge.models.Post;
+import com.example.jamjudge.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    // Retrieve post based on ID
+    @GetMapping(value = "/posts/{postId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getPostById(@PathVariable long postId) throws NoResourceFoundException {
+
+        Post post = postRepository.findById(postId).orElse(null);
+
+        if (post == null) {
+            String path = "/api/users/posts/" + postId;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found");
+        } else {
+            return new ResponseEntity<>(post, HttpStatus.OK);
+        }
+    }
+
+
+
 }
--- src/main/java/com/example/jamjudge/repositories/PostRepository.java
+++ src/main/java/com/example/jamjudge/repositories/PostRepository.java
@@ -1,8 +1,8 @@
 package com.example.jamjudge.repositories;
 
-import com.example.jamjudge.models.UserProfile;
+import com.example.jamjudge.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<UserProfile, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
