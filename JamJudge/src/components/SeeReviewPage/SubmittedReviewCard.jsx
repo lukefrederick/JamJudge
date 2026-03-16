b6cfd14becce0a3bdb6e43a2ff1c67f956b6638d
lukefrederick <129812402+lukefrederick@users.noreply.github.com>
2026-03-16 17:21:24 -0500

    Delete functionality added

--- JamJudge/src/components/SeeReviewPage/SubmittedReviewCard.jsx
+++ JamJudge/src/components/SeeReviewPage/SubmittedReviewCard.jsx
@@ -1,13 +1,12 @@
 function SubmittedReviewCard( { albumName, artistName, rating, reviewContent, onDelete, deleteReviewBool }) {
 
-
     return(
         <div className="reviewCard">
             <h2>Album: {albumName}</h2>
             <h3>Artist: {artistName}</h3>
             <h3>Rating: {rating}</h3>
             <h3>Review: {reviewContent}</h3>
-            {deleteReviewBool && <button onClick={onDelete}>Delete</button>} {/* Will only render if deleteReviewBool is true */}
+            <button onClick={onDelete}>Delete</button>
         </div>
     )
 }
